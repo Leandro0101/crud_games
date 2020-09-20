bd4d8572c452c4f6e47614ef09322e9e0c519640
Leandro0101 <leandrovieira3306@gmail.com>
2020-09-20 02:38:21 -0300

    add web, authentication jwt completed

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,12 +3,13 @@ const routes = express.Router();
 const GameController = require("./controllers/GameController");
 const UserController = require("./controllers/UserController");
 const AuthController = require("./controllers/AuthController");
-routes.post("/game", GameController.store);
+const auth = require("../src/middlwares/auth");
+routes.post("/game", auth, GameController.store);
 routes.get("/games", GameController.index)
 routes.get("/game/:id", GameController.single);
-routes.put("/game/:id", GameController.update);
-routes.delete("/game/:id", GameController.delete);
+routes.put("/game/:id", auth, GameController.update);
+routes.delete("/game/:id", auth, GameController.delete);
 
-routes.post("/user", UserController.store);
+routes.post("/user", auth, UserController.store);
 routes.post("/authenticate", AuthController.authentication);
 module.exports = routes;
